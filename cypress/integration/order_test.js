7dfab9df5dc672d8d871f4c06edb0dd153e930eb
Hailey Magoline <magoline.hailey@gmail.com>
2021-12-17 14:37:48 -0600

    finished writing READme cypress tests.

--- cypress/integration/order_test.js
+++ cypress/integration/order_test.js
@@ -15,7 +15,58 @@ describe('Order App', () => {
 
       const nameInput = () => cy.get('input[id=name-input]');
       const specialInput = () => cy.get('input[name=special]');
-      const submitBtn = () => cy.get('button[order-button]');
-      
+      const submitBtn = () => cy.get('button[id=order-button]');
+      const pepperoni = () => cy.get('input[name=pepperoni]');
+      const sausage = () => cy.get('input[name=sausage]');
+      const extraCheese = () => cy.get('input[name=extraCheese]');
+      const threeCheese = () => cy.get('input[name=threeCheese]');
+      const sizeInput = () => cy.get('select[id=size-dropdown]');
+
+      it('can type in name and special input fields', () => {
+        nameInput()
+        .should('have.value', '')
+        .type('name')
+        .should('have.value', 'name')
+        
+
+        specialInput()
+        .should('have.value', '')
+        .type('special input')
+        .should('have.value', 'special input')
+       
+      })
+
+      it('can select multiple toppings', () => {
+
+        pepperoni().check()
+        sausage().check()
+        threeCheese().check()
+        extraCheese().check()
+
+      })
+
+      it('can submit form', () => {
+
+        submitBtn().should('be.disabled')
+
+        nameInput()
+        .type('name')
+
+        sizeInput()
+        .select('small')
+        
+        pepperoni().check()
+        sausage().check()
+
+        specialInput()
+        .type('special input')
+
+        submitBtn().should('not.be.disabled')
+
+        submitBtn().click()
+
+      })
+
+
 })
 
